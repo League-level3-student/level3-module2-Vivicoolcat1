14bff8e69e9c10fb16701f2e18b52b517d2846d0
League Student <league@iMac-9.attlocal.net>
2019-09-05 18:25:18 -0700

    :smile:

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,59 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
+		for (int i = 0; i < eggs.size(); i++) {
+			if(eggs.get(i).equals("cracked")) {
+				return i;
+			}
+			
+		}
 		return 0;
 	}
+	
+	public static int countPearls(List<Boolean> oysters) {
+		int pearl =0;
+		for (int i = 0; i < oysters.size(); i++) {
+			if(oysters.get(i).equals(true)) {
+				pearl++;
+			}
+			
+		}
+		return pearl;
+	}
+	public static double findTallest(List<Double> peeps) {
+		double tallest = 0;
+		for (int i = 0; i < peeps.size(); i++) {
+			if(peeps.get(i)>tallest) {
+				tallest = peeps.get(i);
+			}
+			
+		}
+		return tallest;
+		
+	}
+	public static String findLongestWord(List<String> words) {
+String longest = "";
+for (int i = 0; i < words.size(); i++) {
+	if(words.get(i).length()>longest.length()) {
+		longest=words.get(i);
+		
+	}
+	
+	
+}
+return longest;
+		
+		
+}
+	
+	public static Boolean containsSOS(List<String> message1) {
+		for (int i = 0; i < message1.size(); i++) {
+			if(message1.get(i).contains("... --- ...")) {
+				return true;
+			}
+			
+		}
+		return false;
+	}
+	
 }
--- src/_02_More_Algorithms/_00_MoreAndMoreTests.java
+++ src/_02_More_Algorithms/_00_MoreAndMoreTests.java
@@ -9,53 +9,87 @@ import org.junit.Test;
  * **/
 
 public class _00_MoreAndMoreTests {
+	public String multiply(int x,int y) {
+		int z= x*y;
+		String s = x + " x " + y + " = " + z;
+		return s;
+		
+	}
+	@Test
+	  public void MultTest() {
 
-//	@Test
-//	  public void MultTest() {
-//
-//	    assertEquals("10 x 0 = 0", multiply(10, 0));
-//	    assertEquals("10 x 10 = 100", multiply(10, 10));
-//	    assertEquals("8 x 11 = 88", multiply(8, 11));
-//	  }
-//
-//	@Test
-//	  public void PrimeTest() {
-//
-//	    assertTrue(isPrime(3));
-//	    assertTrue(isPrime(5));
-//	    assertTrue(isPrime(541));
-//	    assertFalse(isPrime(4));
-//	    assertFalse(isPrime(12));
-//	    assertFalse(isPrime(527));
-//
-//	  }
-//
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
-//	@Test
-//	  public void CubeTest() {
-//
-//	    assertTrue(isCube(27));
-//	    assertTrue(isCube(216));
-//	    assertTrue(isCube(729));
-//	    assertTrue(isCube(1));
-//	    assertFalse(isCube(3));
-//	    assertFalse(isCube(22));
-//	    assertFalse(isCube(143));
-//
-//	  }
+	    assertEquals("10 x 0 = 0", multiply(10, 0));
+	    assertEquals("10 x 10 = 100", multiply(10, 10));
+	    assertEquals("8 x 11 = 88", multiply(8, 11));
+	  }
+	
+	
+public boolean isPrime(int y) {
+for (int i = 2; i < y; i++) {
+	if(y%i==0) {
+		return false;
+	}
+}
+		return true;
+	
+
+	
+}
+	@Test
+	  public void PrimeTest() {
+
+	    assertTrue(isPrime(3));
+	    assertTrue(isPrime(5));
+	    assertTrue(isPrime(541));
+	    assertFalse(isPrime(4));
+	    assertFalse(isPrime(12));
+	    assertFalse(isPrime(527));
+
+	  }
+public Boolean isSquare(int x) {
+	for (int i = 0; i <= x; i++) {
+		if(i*i==x) {
+			return true;
+		}
+		
+	}
+	return false;
+}
+	@Test
+	  public void SquareTest() {
+
+	    assertTrue(isSquare(4));
+	    assertTrue(isSquare(144));
+	    assertTrue(isSquare(64));
+	    assertTrue(isSquare(10201));
+	    assertTrue(isSquare(1));
+	    assertFalse(isSquare(3));
+	    assertFalse(isSquare(22));
+	    assertFalse(isSquare(143));
+
+	  }
+	public Boolean isCube(int x) {
+		for (int i = 0; i <= x; i++) {
+			if(i*i*i==x) {
+				return true;
+			}
+			
+		}
+		return false;
+	}
+
+	@Test
+	  public void CubeTest() {
+
+	    assertTrue(isCube(27));
+	    assertTrue(isCube(216));
+    assertTrue(isCube(729));
+    assertTrue(isCube(1));
+	    assertFalse(isCube(3));
+	    assertFalse(isCube(22));
+    assertFalse(isCube(143));
+
+	  }
 
 
 
